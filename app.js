ac419814dfa1af16b45434c5f304f60299a77bd0
David Scheuermann <dsmann1212@gmail.com>
2017-12-26 16:02:08 -0600

    Rebuilt angular

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ mongoose.connection.on('error', (err) => {
 });
 
 //Port Number
-// const port = 3000;
-const port = process.env.PORT || 8080
+// const port = 5000;
+const port = process.env.PORT || 8080;
 
 //CORS Middleware
 app.use(cors());
--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
+    // database: 'mongodb://localhost:27017/logd',
     database: 'mongodb://david:david@ds131697.mlab.com:31697/logd',
     secret: 'thisisasecret'
 }
