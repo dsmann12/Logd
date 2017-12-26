80a65b95d58af2171087995b5ffaebc1cb67fd22
David Scheuermann <dsmann1212@gmail.com>
2017-12-26 15:36:50 -0600

    Changed database credentials

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://david:1Mannfor23?@ds131697.mlab.com:31697/logd',
+    database: 'mongodb://david:david@ds131697.mlab.com:31697/logd',
     secret: 'thisisasecret'
 }
