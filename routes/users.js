a71d9af42ef02d3e4948d74cb10bcc7c3c1d2ece
David Scheuermann <dsmann1212@gmail.com>
2018-01-05 19:12:40 -0600

    Added image uploads to s3

--- routes/users.js
+++ routes/users.js
@@ -167,13 +167,60 @@ router.post('/avatar', [passport.authenticate('jwt', {session:false}), middlewar
             //console.log(req);
             console.log(req.file);
             console.log(req.file.path);
-            res.json({
-                success: true,
-                msg: 'Image uploaded!',
-                avatar: req.file.filename
+
+            var s3 = require('s3');
+
+            var client = s3.createClient({
+            maxAsyncS3: 20,     // this is the default
+            s3RetryCount: 3,    // this is the default
+            s3RetryDelay: 1000, // this is the default
+            multipartUploadThreshold: 20971520, // this is the default (20 MB)
+            multipartUploadSize: 15728640, // this is the default (15 MB)
+            s3Options: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                region: "us-east-1",
+                // endpoint: 's3.yourdomain.com',
+                // sslEnabled: false
+                // any other options are passed to new AWS.S3()
+                // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+            },
+            });
+
+            console.log('//////////////////////////////////////////////////////');
+            // console.log(req);
+
+            var params = {
+                localFile: req.file.path,
+            
+                s3Params: {
+                Bucket: process.env.S3_BUCKET,
+                Key: req.file.filename,
+                ACL: 'public-read'
+                // other options supported by putObject, except Body and ContentLength.
+                // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+                },
+            };
+
+            var uploader = client.uploadFile(params);
+            uploader.on('error', function(err) {
+                console.error("unable to upload:", err.stack);
+            });
+            uploader.on('progress', function() {
+                console.log("progress", uploader.progressMd5Amount,
+                        uploader.progressAmount, uploader.progressTotal);
+            });
+            uploader.on('end', function() {
+                console.log("done uploading");
+                res.json({
+                    success: true,
+                    msg: 'Image uploaded!',
+                    avatar: req.file.filename
+                });
             });
         }
     });
+
 });
 
 router.get('/:id/backlog/:gameId', (req, res) => {
