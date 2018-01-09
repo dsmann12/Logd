d047000bf885dc551b1d52101728166928cdaf93
David Scheuermann <dsmann1212@gmail.com>
2018-01-08 20:21:47 -0600

    not use doc in jwt payload

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = function(passport){
   opts.secretOrKey = config.secret;
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
     console.log(jwt_payload);
-    User.getUserById(jwt_payload._doc._id, (err, user) => {
+    User.getUserById(jwt_payload._id, (err, user) => {
       
       if(err){
         console.log('err');
